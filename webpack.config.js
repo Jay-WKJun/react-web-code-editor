0a3edaa03aab8419d5ed55f859efd0c0c27d5913
Woo KyungJun <natcho9010@gmail.com>
2022-01-13 17:58:55 +0900

    Config : webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.tsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/,
+				use: ['ts-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	devServer: {
+		port: 3000,
+		open: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+	],
+};
