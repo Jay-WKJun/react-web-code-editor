3ff0e20fa94637548c5bc482e625185f57ab1dfc
Woo KyungJun <natcho9010@gmail.com>
2022-01-14 14:38:00 +0900

    Add : textArea & pre component

--- src/Containers/App/App.tsx
+++ src/Containers/App/App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 200px;
+  height: 200px;
+	padding: 0;
+`;
+
+const TextArea = styled.textarea`
+  position: absolute;
+  top: 0;
+  left: 0;
+	width: inherit;
+	height: inherit;
+	padding: 10px;
+`;
+
+const Pre = styled.pre`
+  position: relative;
+	padding: 10px;
+	width: inherit;
+	height: inherit;
+	margin: 0px;
+	box-sizing: border-box;
+`;
 
 function App() {
+	const [text, setText] = useState();
+
+	const handleTextChange = useCallback((e) => {
+		const value = e.currentTarget.value;
+		setText(value);
+	}, []);
+
   return (
-    <div>
-      test
-    </div>
+    <Wrapper>
+      <TextArea value={text} onChange={handleTextChange} />
+	    <Pre>{text}</Pre>
+    </Wrapper>
   );
 }
 
