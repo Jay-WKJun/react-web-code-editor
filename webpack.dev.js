4e02590b4c5190c8a508bebdeba179bf5163a08c
Woo KyungJun <natcho9010@gmail.com>
2022-01-23 23:19:08 +0900

    Chore : add production setting

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
 	entry: './dev/index.tsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+	entry: './src/index.tsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+    library: {
+      name: 'light-code-editor',
+      type: 'umd',
+      export: 'default',
+    },
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/,
+				use: ['ts-loader'],
+				exclude: /node_modules/,
+			},
+		],
+	},
+  plugin: [
+    new CleanWebpackPlugin(),
+  ],
+};
