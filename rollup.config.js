e6d1205d42b59988b1b508423ff7d6a29011dc05
Woo KyungJun <natcho9010@gmail.com>
2022-05-05 22:02:47 +0900

    Chore : add TypeScript plugin & visualizer plugin & set plugin order

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import babel from '@rollup/plugin-babel';
-// external /node_modules/ 를 대신 해주는 것
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-// commonjs로 되어있는 파일들을 esm으로 변환시켜주는 plugin
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import { visualizer } from 'rollup-plugin-visualizer';
+import typescript from 'rollup-plugin-typescript2';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 const inputSrc = [
-  ['./index.ts', 'es'],
+  ['./index.ts', 'esm'],
   ['./index.ts', 'cjs'],
 ];
 
@@ -27,18 +27,28 @@ export default inputSrc
     },
     external: [/@babel\/runtime/],
     plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      typescript({
+        compilerOptions: {
+          outDir: `dist/${format}`,
+          declarationDir: `dist/${format}`,
+        },
+        exclude: ['*.d.ts', '**/*.d.ts'],
+      }),
+      commonjs({
+        extensions,
+      }),
       babel({
         babelHelpers: 'runtime',
         exclude: '**/node_modules/**',
         extensions,
       }),
-      nodeResolve({
-        extensions,
-      }),
-      commonjs({
-        extensions,
-      }),
       peerDepsExternal(),
       postcss(),
+      visualizer({
+        filename: 'stats.html',
+      }),
     ],
   }));
